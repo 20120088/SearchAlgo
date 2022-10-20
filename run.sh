ad89bc49310d3afec7c0bc03a66da8fbec430c9f
Hoang <20120088@student.hcmus.edu.vn>
2022-10-20 13:38:50 +0700

    Add main.py call in run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+cd source
+# python main.py dfs
+python main.py bfs
+python main.py ucs
+python main.py gbfs manhattan_distance
+python main.py gbfs euclidean_distance
+python main.py gbfs chebyshev_distance
+python main.py astar manhattan_distance
+python main.py astar euclidean_distance
+python main.py astar chebyshev_distance
+echo "Finish"
