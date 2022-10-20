8b4d2266b7dc7bb521f7b114e572958eaa85d7f9
Hoang <20120088@student.hcmus.edu.vn>
2022-10-20 20:59:34 +0700

    Functionalize print_table.py

--- source/print_table.py
+++ source/print_table.py
@@ -1,26 +1,21 @@
 table = open('table.csv', 'r').readlines()
 header = table[0][:-1].split(',')
 
-print(' ', end = '')
-for i in range(37 + 20 * len(header[2:])): print('-', end='')
-print()
+def print_line():
+    print(' ', end = '')
+    for i in range(37 + 23 * len(header[2:])): print('-', end='')
+    print()
 
-print('|  {:<12}|| {:<20}'.format(header[0], header[1]),end='')
-for h in header[2:]:
-    print('|  {:<17}'.format(h), end='')
-print('|')
-
-print(' ', end = '')
-for i in range(37 + 20 * len(header[2:])): print('-', end='')
-print('')
-
-for row in table[1:]:
-    row = row[:-1].split(',')
-    print('|  {:<12}|| {:<20}'.format(row[0], row[1]), end='')
-    for item in row[2:]:
-        print('|  {:<17}'.format(item), end='')
+def print_row(row):
+    print('|  {:<12}|| {:<20}'.format(row[0], row[1]),end='')
+    for h in row[2:]:
+        print('|  {:<20}'.format(h), end='')
     print('|')
 
-print(' ', end = '')
-for i in range(37 + 20 * len(header[2:])): print('-', end='')
-print('')
+print_line()
+print_row(header)
+print_line()
+for row in table[1:]:
+    row = row[:-1].split(',')
+    print_row(row)
+print_line()
